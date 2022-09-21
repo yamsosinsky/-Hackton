59e7ecba15bc6490bf99eee8607b05aade1977c5
yamsosinsky <yamsosinsky@gmail.com>
2022-09-21 15:29:35 +0300

    update

--- module.py
+++ module.py
@@ -5,18 +5,25 @@ pygame.font.init()
 
 
 def print_question(list_of_questions):
-    random_index = random.randint(0, len(list_of_questions) - 1)
+    random_index = random.randint(0, 14)
+    x = 35
+    y = 110
     screen = pygame.display.set_mode(const.SIZE)
     pygame.display.set_caption("QUESTION")
     screen.fill(const.WHITE)
     random_question = list_of_questions.pop(random_index)
-    question_text = const.QUESTION_FONT.render(random_question, 1, const.BLACK)
+    random_question = random_question.split("\n")
+    for i in range(len(random_question)):
+        text = random_question[i]
+        question_text = const.QUESTION_FONT.render(text, 1, const.BLACK)
+        screen.blit(question_text, (30, y))
+        y += 50
     true_text = const.ANSWEAR_FONT.render("TRUE", 1, const.BLACK)
     false_text = const.ANSWEAR_FONT.render("FALSE", 1, const.BLACK)
-    screen.blit(question_text, (30, 210))
     screen.blit(true_text, (80, 410))
     screen.blit(false_text, (400, 410))
     pygame.display.flip()
+    pygame.time.wait(1000)
     return random_index
 
 
@@ -39,5 +46,17 @@ def check_answer(random_index, question_dict, list_of_questions):
         return False
 
 
-def open_question():
-    pass
+def open_question(list_loc):
+    if list_loc[0] == 140 and list_loc[1] == 560:
+        random_index = print_question(const.list_of_questions)
+        return check_answer(random_index, const.dict_of_questions, const.list_of_questions)
+    if list_loc[1] == 420 and (list_loc[0] == 280 or list_loc[0] == 560):
+        random_index = print_question(const.list_of_questions)
+        return check_answer(random_index, const.dict_of_questions, const.list_of_questions)
+    if list_loc[1] == 140 and list_loc[0] == 280:
+        random_index = print_question(const.list_of_questions)
+        return check_answer(random_index, const.dict_of_questions, const.list_of_questions)
+    if list_loc[1] == 0 and (list_loc[0] == 140 or list_loc[0] == 420):
+        random_index = print_question(const.list_of_questions)
+        return check_answer(random_index, const.dict_of_questions, const.list_of_questions)
+
